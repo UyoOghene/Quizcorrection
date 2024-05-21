b35d1596131ca960e543609342799902f69d3bf8
Uyooghene <uyooghene01@gmail.com>
2024-05-21 13:39:20 +0100

    may 20

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let score = 0;
-let currentQuestionIndex = 0;
+import questions from "./questions.js";
+const startBtn = document.getElementById('start');
 const answerButtons = document.getElementById('answer-buttons');
 const quizQuestions = document.querySelector('.quizquestions');
 const question = document.getElementById('question');
@@ -11,7 +11,6 @@ const musictxt = document.querySelector('#musictxt');
 const music = document.querySelector('.music');
 const musicSrc = document.querySelector('#music-src');
 const startPage = document.querySelector('#start-page');
-const startBtn = document.getElementById('start');
 const hallbtn = document.querySelector('#hall');
 const quizBox = document.querySelector('.quizbox');
 const backButton = document.querySelector('#back');
@@ -25,257 +24,293 @@ const hallInput = document.querySelector('#hallinput');
 const inputBtn = document.querySelector('#inputBtn');
 const playagainBtn = document.querySelector('#playagainbtn');
 
-import questions from "./questions.js";
-
-let countdown = 10; 
-let highScoreName = hallInput.value;
-let mytimeout = setTimeout(time, 2000);
-
+let score = 0;
+let currentQuestionIndex = 0;
+let countdown = 10;
 startBtn.addEventListener('click', startQuiz);
-hallInput.addEventListener('keydown', showInputValue2);
-backButton.addEventListener('click',backbtn);
-playagainBtn.addEventListener('click',playAgain);
-hallbtn.addEventListener('click', fame);
-next.addEventListener('click', nextQ);
-inputBtn.addEventListener('click', saveHighScore);
+// hallInput.addEventListener('keydown', showInputValue2);
+// backButton.addEventListener('click',backbtn);
+// playagainBtn.addEventListener('click',playAgain);
+// hallbtn.addEventListener('click', fame);
+// next.addEventListener('click', nextQ);
+// inputBtn.addEventListener('click', saveHighScore);
 // musictxt.addEventListener('click', changeSrc);
 
-function starterPageFunc(){
-}
-
 function startQuiz() {
+    let intervalId = setInterval(time, 1000);// let mytimeout = setTimeout(time, 1000);
     startPage.style.display = "none";
-    console.log('Starting quiz');
     quizBox.style.display = "block";
     quizQuestions.style.display = 'block';
-    inputBtn.style.display = 'none';
-    highScoreElement.style.display = 'none'; 
-    countdown = 10;
-    time(); 
-    currentQuestionIndex = 0;
-    score = 0;
-    presentScore.innerHTML = 0;
-    questions.sort(() => Math.random() - 0.5);
-    const selectedQuestions = questions.slice(0, 5);
-    questions.splice(0, questions.length, ...selectedQuestions);
-    next.innerHTML = 'Next';
+    time(intervalId);
+    // currentQuestionIndex = 0;
+    // score = 0;
+    selectQuestions();
+    // presentScore.innerHTML = 0;
+    // next.innerHTML = 'Next';
     showQuestion();
 }
 
-function backbtn(){
-    console.log('back');
-    startPage.style.display = "block";
-    inputBtn.style.display = 'none';
-    hallInput.style.display = 'none';
-    quizBox.style.display = "none";
-    highScoreElement.style.display = 'none';
-    playagainBtn.style.display = 'none';
+function selectQuestions(){
+    questions.sort(() => Math.random() - 0.5);
+    const selectedQuestions = questions.slice(0, 5);
+    questions.splice(0, questions.length, ...selectedQuestions);
+
 }
 
-function playAgain(){
-    console.log('back');
-    hallInput.style.display = 'none';
-    inputBtn.style.display = 'none';
-    highScoreElement.style.display = 'none';
-    startPage.style.display = "block";
-    quizBox.style.display = "none"; 
-    playagainBtn.style.display = 'none';
-}
+// function backbtn(){
+//     console.log('back');
+//     startPage.style.display = "block";
+//     inputBtn.style.display = 'none';
+//     hallInput.style.display = 'none';
+//     quizBox.style.display = "none";
+//     highScoreElement.style.display = 'none';
+//     playagainBtn.style.display = 'none';
+// }
 
-function saveHighScore() {
-    let highScoreName = hallInput.value.trim();
-    if (highScoreName) {
-        const highScores = getHighScores();
-        highScores.push({ name: highScoreName, score: score });
-        highScores.sort((a, b) => b.score - a.score);
-        highScores.splice(5);
-        localStorage.setItem('highScores', JSON.stringify(highScores));
-        displayHighScores();
-    }
-}
+// function playAgain(){
+//     console.log('back');
+//     hallInput.style.display = 'none';
+//     inputBtn.style.display = 'none';
+//     highScoreElement.style.display = 'none';
+//     startPage.style.display = "block";
+//     quizBox.style.display = "none"; 
+//     playagainBtn.style.display = 'none';
+// }
 
-function getHighScores() {
-    const scores = localStorage.getItem('highScores');
-    return scores ? JSON.parse(scores) : [];
-}
+// function saveHighScore() {
+//     let highScoreName = hallInput.value.trim();
+//     if (highScoreName) {
+//         const highScores = getHighScores();
+//         highScores.push({ name: highScoreName, score: score });
+//         highScores.sort((a, b) => b.score - a.score);
+//         highScores.splice(5);   
+//         localStorage.setItem('highScores', JSON.stringify(highScores));
+//         displayHighScores();
+//     }
 
-function displayHighScores() {
-    const highScores = getHighScores().slice(0, 5);  
-    const highScoreElements = [highScore1, highScore2, highScore3, highScore4, highScore5];
-    highScores.forEach((score, index) => {
-        if (highScoreElements[index]) {
-            highScoreElements[index].textContent = `${Object.values(score)[0]}: ${Object.values(score)[1]}`;
+//     inputBtn.style.display = 'none';
+//     hallInput.style.display = 'none';
+// }
+
+// function getHighScores() {
+//     const scores = localStorage.getItem('highScores');
+//     return scores ? JSON.parse(scores) : [];
+// }
+
+// function displayHighScores() {
+//     const highScores = getHighScores().slice(0, 5);  
+//     const highScoreElements = [highScore1, highScore2, highScore3, highScore4, highScore5];
+//     highScores.forEach((score, index) => {
+//         if (highScoreElements[index]) {
+//             highScoreElements[index].textContent = `${Object.values(score)[0]}: ${Object.values(score)[1]}`;
+//         }
+//     });
+
+//     for (let i = highScores.length; i < highScoreElements.length; i++) {
+//         if (highScoreElements[i]) {
+//             highScoreElements[i].textContent = '';
+//         }
+//     }
+// }
+
+// function fame() {
+//     console.log('Hall of Fame');
+//     quizBox.style.display = 'none';
+//     startPage.style.display = 'none';
+//     highScoreElement.style.display = 'block'; 
+//     displayHighScores();
+// } 
+
+// function time() {
+//     timer.innerHTML = 'Time left:' + countdown + 's';
+//     countdown -= 1; 
+//     if (countdown < 0) {
+//         timer.innerHTML = 'Oops! Time\'s up'; 
+//         Array.from(answerButtons.children).forEach(button => {
+//             if (button.dataset.correct === 'true') {
+//                 button.classList.add('correct');
+//             }
+//             button.disabled = true;
+//         });
+//         next.style.display = 'block';
+//     } else {
+//        let mytimeout = setTimeout(time, 1000);
+//         // mytimeout;
+//     }
+// }
+
+function times(){
+     intervalId = setInterval(() => {
+        timer.innerHTML = 'Time left:' + countdown + 's';
+        console.log('tick');
+        if (countdown === 0) {
+            Array.from(answerButtons.children).forEach(button => {
+                if (button.dataset.correct === 'true') {
+                    button.classList.add('correct');
+                }
+                button.disabled = true;
+            });
+            next.style.display = 'block';
+            timer.innerHTML = 'Oops! Time\'s up'; 
+            clearInterval(intervalId);
+        } else {
+            countdown--;
         }
-    });
-
-    for (let i = highScores.length; i < highScoreElements.length; i++) {
-        if (highScoreElements[i]) {
-            highScoreElements[i].textContent = '';
-        }
-    }
+    }, 1000);
 }
 
-function fame() {
-    console.log('Hall of Fame');
-    quizBox.style.display = 'none';
-    startPage.style.display = 'none';
-    highScoreElement.style.display = 'block'; 
-    displayHighScores();
-} 
+function time(intervalId){
+    timer.innerHTML = 'Time left:' + countdown + 's';
 
-function time() {
-    timer.innerHTML = 'Time left:' + countdown + 's' ; 
-    countdown -=1; 
-    if (countdown < 0) {
+    console.log('tick')
+    if (countdown === 0) {
+        clearInterval(intervalId);
         timer.innerHTML = 'Oops! Time\'s up'; 
-        Array.from(answerButtons.children).forEach(button => {
-            if (button.dataset.correct === 'true') {
-                button.classList.add('correct');
-            }
-            button.disabled = true;
-        });
+        checkCorrectAns();
         next.style.display = 'block';
     } else {
-        mytimeout = setTimeout(time, 2000);
-    }
-}
- 
-function showInputValue2(e){
-
-        if (e.key === 'Enter'  ) {
-            console.log('Enter key was pressed.');
-            inputBtn.style.display = 'none';
-            console.log(hallInput.value);
-            hallInput.style.display = 'none';
-        }
-        
-    }
-    
-
-function showQuestion(){
-    
-    clearTimeout(mytimeout);
-    timer.style.display ='block';
-    resetState();
-    countdown = 10;
-    time();
-    let currentQuestion =questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex +1;
-    questionnum.innerHTML ='Question' + ' ' + questionNo + ' ' + 'out of 5 shuffled from 10';
-    question.innerHTML =questionNo +'. '+ currentQuestion.question;
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerHTML = answer.text;
-        button.classList.add('btn');
-        answerButtons.appendChild(button);
-        if(answer.correct){
-            button.dataset.correct =answer.correct;
-            clearTimeout(mytimeout);
-
-        }
-    button.addEventListener('click',selectAnswer);
-    });
-    next.style.display = 'none';
-}
-
-function resetState(){
-    while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.firstChild)
+        countdown--;
     }
 }
 
-function selectAnswer(e){
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === 'true';
-    if(isCorrect){
-        selectedBtn.classList.add('correct');
-        score++;
-    }else{
-        selectedBtn.classList.add('incorrect');
-    }
-
+function checkCorrectAns(){
     Array.from(answerButtons.children).forEach(button => {
-        if(button.dataset.correct === 'true'){
+        if (button.dataset.correct === 'true') {
             button.classList.add('correct');
         }
         button.disabled = true;
+    });
 
-    }); 
-    timer.style.display ='none';
-    next.style.display = 'block';
 }
-     
-function showScore() {
-    console.log('showscore')
-    console.log(hallInput.value)
-    quizQuestions.style.display = 'none';
-    inputBtn.style.display = 'block'
-    const totalScore = document.createElement('h1');
-    totalScore.setAttribute('id', 'total-score');
-    quizBox.appendChild(totalScore);
-    totalScore.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    const hallOfFameBtn = document.createElement('button');
-    hallOfFameBtn.innerText = 'Hall of Fame';
-    hallOfFameBtn.classList.add('btn');
-    hallOfFameBtn.addEventListener('click', fame);
-    playagainBtn.style.display = 'block';
-    next.style.display = 'none';
-    quizBox.appendChild(hallOfFameBtn); 
-    quizBox.appendChild(next);
-}
+// function showInputValue2(e){
+//         if (e.key === 'Enter') {
+//             hallInput.style.display = 'none';
+//             inputBtn.style.display = 'none';
+//         }   
 
-function handleNxtbtn() {
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions.length) {
-        clearTimeout(mytimeout);
-        showQuestion();
-    } else {
-        hallInput.style.display = 'block';
-        timer.style.display= 'none';
-        clearTimeout(mytimeout);
-        console.log('quiz end')
-        showScore();
-    }
-}
-
-
-function nextQ(){
-    clearTimeout(mytimeout);
-    timer.innerHTML = '';
-    presentScore.innerHTML = score;
-    if (currentQuestionIndex < questions.length) {
-        handleNxtbtn();
-        countdown = 10;
-        time();
-
-    } else {
-        clearTimeout(mytimeout);
-        startQuiz();
-        clearTimeout(mytimeout);
-
-        timer.style.display='block';
-        countdown = 10;
-        time();
+//     }
     
-    }
+function showQuestion(){  
+    // clearTimeout(mytimeout);
+    // clearInterval(intervalId);
+    console.log('hello');
+    // removeAnswerBtns();
+    // countdown = 10;
+    // time();
+    // let currentQuestion =questions[currentQuestionIndex];
+    // let questionNo = currentQuestionIndex +1;
+    // questionnum.innerHTML ='Question' + ' ' + questionNo + ' ' + 'out of 5 shuffled from 10';
+    // question.innerHTML =questionNo +'. '+ currentQuestion.question;
+
+    // currentQuestion.answers.forEach(answer => {
+    //     const button = document.createElement('button');
+    //     button.innerHTML = answer.text;
+    //     button.classList.add('btn');
+    //     answerButtons.appendChild(button);
+    //     if(answer.correct){
+    //         button.dataset.correct =answer.correct;
+    //         // clearTimeout(mytimeout);
+    //     }
+    // button.addEventListener('click',selectAnswer);
+    // });
+    // next.style.display = 'none';
 }
 
+// function removeAnswerBtns(){
+//     while(answerButtons.firstChild){
+//         answerButtons.removeChild(answerButtons.firstChild)
+//     }
+// }
 
-function changeSrc() {
-    console.log(musicSrc);
+// function selectAnswer(e){
+//     const selectedBtn = e.target;
+//     const isCorrect = selectedBtn.dataset.correct === 'true';
+//     if(isCorrect){
+//         selectedBtn.classList.add('correct');
+//         score++;
+//     }else{
+//         selectedBtn.classList.add('incorrect');
+//     }
 
-    let changeattribute = musicSrc.getAttribute('src');
-    console.log(changeattribute);
-    if (changeattribute.includes('./music/French Montana - Ain\'t Worried About Nothin (Explicit).mp3')) {
-        console.log('ayra');
-        musicSrc.setAttribute("src", './music/Rema-Calm-Down.mp3');
-        console.log(musicSrc);
-        musicSrc.play();
-    } else {
-        console.log('french');
-        musicSrc.setAttribute("src", "./music/French Montana - Ain\'t Worried About Nothin (Explicit).mp3");
-        musicSrc.play();
-    }
-}
+//     Array.from(answerButtons.children).forEach(button => {
+//         if(button.dataset.correct === 'true'){
+//             button.classList.add('correct');
+//         }
+//         button.disabled = true;
+
+//     }); 
+//     timer.style.display ='none';
+//     next.style.display = 'block';
+// }
+     
+// function showScore() {
+//     quizQuestions.style.display = 'none';
+//     inputBtn.style.display = 'block';
+//     const totalScore = document.createElement('h1');
+//     totalScore.setAttribute('id', 'total-score');
+//     quizBox.appendChild(totalScore);
+//     totalScore.innerHTML = `You scored ${score} out of ${questions.length}!`;
+//     if (!document.getElementById('hall-of-fame-btn')) {
+//         const hallOfFameBtn = document.createElement('button');
+//         hallOfFameBtn.setAttribute('id', 'hall-of-fame-btn');  // Add an ID to the button
+//         hallOfFameBtn.innerText = 'Hall of Fame';
+//         hallOfFameBtn.classList.add('btn');
+//         hallOfFameBtn.addEventListener('click', fame);
+//         quizBox.appendChild(hallOfFameBtn);
+//     }
+//     playagainBtn.style.display = 'block';
+//     next.style.display = 'none';
+//     quizBox.appendChild(next);
+// }
+
+// function handleNxtbtn() {
+//     currentQuestionIndex++;
+//     if (currentQuestionIndex < questions.length) {
+//         clearTimeout(mytimeout);
+//         showQuestion();
+//     } else {
+//         hallInput.style.display = 'block';
+//         timer.style.display= 'none';
+//         clearTimeout(mytimeout);
+//         console.log('quiz end')
+//         showScore();
+//     }
+// }
+
+// function nextQ(){
+//     clearTimeout(mytimeout);
+//     timer.innerHTML = '';
+//     presentScore.innerHTML = score;
+//     if (currentQuestionIndex < questions.length) {
+//         handleNxtbtn();
+//         countdown = 10;
+//         time();
+
+//     } else {
+//         clearTimeout(mytimeout);
+//         startQuiz();
+//         clearTimeout(mytimeout);
+//         timer.style.display='block';
+//         countdown = 10;
+//         time();
+    
+//     }
+// }
+
+// function changeSrc() {
+//     console.log(musicSrc);
+
+//     let changeattribute = musicSrc.getAttribute('src');
+//     console.log(changeattribute);
+//     if (changeattribute.includes('./music/French Montana - Ain\'t Worried About Nothin (Explicit).mp3')) {
+//         console.log('ayra');
+//         musicSrc.setAttribute("src", './music/Rema-Calm-Down.mp3');
+//         console.log(musicSrc);
+//         musicSrc.play();
+//     } else {
+//         console.log('french');
+//         musicSrc.setAttribute("src", "./music/French Montana - Ain\'t Worried About Nothin (Explicit).mp3");
+//         musicSrc.play();
+//     }
+// }
 
